9a0f761d5094fa88dfd327b42e054de808641203
kabradshaw1 <itllbok82@gmail.com>
2022-10-03 14:39:15 -0400

    set up routes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,7 +1,27 @@
 const router = require('express').Router();
-const notes = require('../../lib/notes');
 const {db} = require('../../data/db');
 
+function createNewNote(body, dbArray) {
+  const notes = body;
+  dbArray.push(notes);
+  FileSystem.writeFileSync(
+    path.join(__dirname, '../data/db.json'),
+    JSON.stringify({ notes: dbArray }, null, 2)
+  )
+  return notes;
+}
+
+router.get('/api/notes', (req, res) => {
+  res.json(db);
+});
+
+router.post('/api/notes', (req, res) => {
+  req.body.id = db.length.toString();
+
+  const db = createNewNote(req.body, db)
+  res.json(db);
+  
+})
 
 module.exports = router;
 
@@ -9,3 +29,13 @@ module.exports = router;
 // POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return
 // the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm
 // packages that could do this for you
+
+// function createNewAnimal(body, animalsArray) {
+//   const animal = body;
+//   animalsArray.push(animal);
+//   fs.writeFileSync(
+//     path.join(__dirname, '../data/animals.json'),
+//     JSON.stringify({ animals: animalsArray }, null, 2)
+//   );
+//   return animal;
+// }
