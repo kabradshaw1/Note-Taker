1da6119fbcca1b5094ad8d74e7ee60b4a30eefd6
kabradshaw1 <itllbok82@gmail.com>
2022-10-03 13:13:33 -0400

    added some skeleton stuff

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
-const { db } = require('./data/db')
+
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,8 +9,10 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
